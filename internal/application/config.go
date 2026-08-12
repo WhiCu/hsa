40ca865d23350a21b8119ac9d8c71ab1e7f3ca90
WhiCu <maks29032005@gmail.com>
2026-08-12 23:54:16 +0300

    feat(app): added "samber/do" package

--- internal/application/config.go
+++ internal/application/config.go
@@ -1,6 +1,33 @@
 package application
 
+import "time"
+
 type Config struct {
+	Invite  InviteConfig  `koanf:"invite" validate:"required"`
+	Session SessionConfig `koanf:"session" validate:"required"`
 }
 
-// var defaultCfg = Config{}
+type InviteConfig struct {
+	MaxActive int `koanf:"max_active" validate:"required,gt=0"`
+
+	TTL time.Duration `koanf:"ttl" validate:"required,gt=0"`
+
+	MaxWrappedKey int `koanf:"max_wrapped_key" validate:"required,gt=0"`
+}
+
+type SessionConfig struct {
+	RefreshTTL time.Duration `koanf:"refresh_ttl" validate:"required,gt=0"`
+
+	AccessTTL time.Duration `koanf:"access_ttl" validate:"required,gt=0"`
+}
+
+var defaultCfg = Config{
+	Invite: InviteConfig{
+		MaxActive: 3,
+		TTL:       72 * time.Hour,
+	},
+	Session: SessionConfig{
+		RefreshTTL: 30 * 24 * time.Hour,
+		AccessTTL:  15 * time.Minute,
+	},
+}
--- internal/application/package.go
+++ internal/application/package.go
@@ -1,7 +1,354 @@
 package application
 
 import (
+	"log/slog"
+
+	"github.com/knadh/koanf/v2"
 	"github.com/samber/do/v2"
+	"github.com/whicu/hsa/internal/config"
+	"github.com/whicu/hsa/internal/domain/invite"
+	"github.com/whicu/hsa/internal/domain/key"
 )
 
-var Package = do.Package()
+func newConfig(i do.Injector) (Config, error) {
+	k, err := do.Invoke[*koanf.Koanf](i)
+	if err != nil {
+		return Config{}, err
+	}
+	def := defaultCfg
+	return config.GetConfig(k, "app", &def)
+}
+
+func newCreateInvite(i do.Injector) (*CreateInvite, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	invites, err := do.InvokeAs[InviteSaver](i)
+	if err != nil {
+		return nil, err
+	}
+
+	counter, err := do.InvokeAs[ActiveInviteCounter](i)
+	if err != nil {
+		return nil, err
+	}
+
+	tokens, err := do.InvokeAs[TokenGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	ids, err := do.InvokeAs[IDGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	transactor, err := do.InvokeAs[Transactor](i)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := do.Invoke[Config](i)
+	if err != nil {
+		return nil, err
+	}
+	cfgInvite := cfg.Invite
+
+	return NewCreateInvite(
+		log, invites, counter, tokens, ids,
+		invite.NewPolicy(cfgInvite.MaxActive), transactor, cfgInvite.TTL,
+	), nil
+}
+
+func newBeginInviteRegistration(i do.Injector) (*BeginInviteRegistration, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	invites, err := do.InvokeAs[InviteFinderByCode](i)
+	if err != nil {
+		return nil, err
+	}
+
+	ids, err := do.InvokeAs[IDGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	registrator, err := do.InvokeAs[Registrator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	hash, err := do.InvokeAs[HashGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewBeginInviteRegistration(log, invites, ids, registrator, hash), nil
+}
+
+func newSessionIssuer(i do.Injector) (*SessionIssuer, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	sessions, err := do.InvokeAs[RefreshTokenSaver](i)
+	if err != nil {
+		return nil, err
+	}
+
+	refreshTokens, err := do.InvokeAs[TokenGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	accessTokens, err := do.InvokeAs[TokenIssuer](i)
+	if err != nil {
+		return nil, err
+	}
+
+	ids, err := do.InvokeAs[IDGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := do.Invoke[Config](i)
+	if err != nil {
+		return nil, err
+	}
+
+	cfgSession := cfg.Session
+
+	return NewSessionIssuer(
+		log,
+		sessions,
+		refreshTokens,
+		accessTokens,
+		ids,
+		cfgSession.RefreshTTL,
+		cfgSession.AccessTTL,
+	), nil
+}
+
+func newFinishInviteRegistration(i do.Injector) (*FinishInviteRegistration, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	invites, err := do.InvokeAs[InviteFinderByID](i)
+	if err != nil {
+		return nil, err
+	}
+
+	users, err := do.InvokeAs[UserSaver](i)
+	if err != nil {
+		return nil, err
+	}
+
+	credentials, err := do.InvokeAs[CredentialSaver](i)
+	if err != nil {
+		return nil, err
+	}
+
+	keys, err := do.InvokeAs[WrappedKeySaver](i)
+	if err != nil {
+		return nil, err
+	}
+
+	sessionIssuer, err := do.Invoke[*SessionIssuer](i)
+	if err != nil {
+		return nil, err
+	}
+
+	registrator, err := do.InvokeAs[Registrator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	ids, err := do.InvokeAs[IDGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	transactor, err := do.InvokeAs[Transactor](i)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg, err := do.Invoke[Config](i)
+	if err != nil {
+		return nil, err
+	}
+	cfgInvite := cfg.Invite
+
+	return NewFinishInviteRegistration(
+		log,
+		invites,
+		users,
+		credentials,
+		keys,
+		sessionIssuer,
+		registrator,
+		ids,
+		transactor,
+		key.NewPolicy(cfgInvite.MaxWrappedKey),
+	), nil
+}
+
+func newBeginLogin(i do.Injector) (*BeginLogin, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	authenticator, err := do.InvokeAs[Authenticator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewBeginLogin(log, authenticator), nil
+}
+
+func newRevokeAllUserSessions(i do.Injector) (*RevokeAllUserSessions, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	sessions, err := do.InvokeAs[ActiveSessionsFinder](i)
+	if err != nil {
+		return nil, err
+	}
+
+	saver, err := do.InvokeAs[RefreshTokenSaver](i)
+	if err != nil {
+		return nil, err
+	}
+
+	transactor, err := do.InvokeAs[Transactor](i)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewRevokeAllUserSessions(log, sessions, saver, transactor), nil
+}
+
+func newLogin(i do.Injector) (*Login, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	credentials, err := do.InvokeAs[CredentialFinder](i)
+	if err != nil {
+		return nil, err
+	}
+
+	credentialSaver, err := do.InvokeAs[CredentialSaver](i)
+	if err != nil {
+		return nil, err
+	}
+
+	authenticator, err := do.InvokeAs[Authenticator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	sessionIssuer, err := do.Invoke[*SessionIssuer](i)
+	if err != nil {
+		return nil, err
+	}
+
+	revokeUser, err := do.Invoke[*RevokeAllUserSessions](i)
+	if err != nil {
+		return nil, err
+	}
+
+	transactor, err := do.InvokeAs[Transactor](i)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewLogin(
+		log,
+		credentials,
+		credentialSaver,
+		authenticator,
+		sessionIssuer,
+		revokeUser,
+		transactor,
+	), nil
+}
+
+func newRefreshAccessToken(i do.Injector) (*RefreshAccessToken, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	sessions, err := do.InvokeAs[RefreshTokenFinder](i)
+	if err != nil {
+		return nil, err
+	}
+
+	revokeUser, err := do.Invoke[*RevokeAllUserSessions](i)
+	if err != nil {
+		return nil, err
+	}
+
+	sessionIssuer, err := do.Invoke[*SessionIssuer](i)
+	if err != nil {
+		return nil, err
+	}
+
+	hasher, err := do.InvokeAs[HashGenerator](i)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewRefreshAccessToken(
+		log,
+		sessions,
+		revokeUser,
+		sessionIssuer,
+		hasher,
+	), nil
+}
+
+func newRevokeCompromisedChain(i do.Injector) (*RevokeCompromisedChain, error) {
+	log, err := do.Invoke[*slog.Logger](i)
+	if err != nil {
+		return nil, err
+	}
+
+	descendants, err := do.InvokeAs[UserDescendantsFinder](i)
+	if err != nil {
+		return nil, err
+	}
+
+	revokeUser, err := do.Invoke[*RevokeAllUserSessions](i)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewRevokeCompromisedChain(log, descendants, revokeUser), nil
+}
+
+var Package = do.Package(
+	do.Lazy(newConfig),
+	do.Lazy(newCreateInvite),
+	do.Lazy(newBeginInviteRegistration),
+	do.Lazy(newFinishInviteRegistration),
+	do.Lazy(newSessionIssuer),
+	do.Lazy(newBeginLogin),
+	do.Lazy(newLogin),
+	do.Lazy(newRefreshAccessToken),
+	do.Lazy(newRevokeAllUserSessions),
+	do.Lazy(newRevokeCompromisedChain),
+)
